d66cc113abc24768b915603bbb127228bce1d204
Keil Reedich <keilreedich@Keils-MacBook-Air.local>
2021-04-29 21:55:06 -0400

    almost there

--- src/confetti.cpp
+++ src/confetti.cpp
@@ -12,12 +12,24 @@ using namespace glm;
 
 void Confetti::createParticles(int size) 
 {
-   mTexture = theRenderer.loadTexture("../textures/particle.png");
-   // TODO: Create particles
+  mTexture = theRenderer.loadTexture("../textures/particle.png");
+  Particle p;
+  p.size = 1;
+  p.pos = random_unit_cube();
+  p.color = vec4(random_float(), random_float(), random_float(), random_float());
+  p.vel = vec3(random_float(), random_float(), random_float());
+  mParticles.push_back(p);
 }
 
 void Confetti::update(float dt)
 {
   // update particles
-}
+  Particle p = mParticles[0];
 
+  if(p.pos.x > 1 || p.pos.x < -1) p.vel.x = -p.vel.x;
+  if(p.pos.y > 1 || p.pos.y < -1) p.vel.y = -p.vel.y;
+  if(p.pos.z > 1 || p.pos.z < -1) p.vel.z = -p.vel.z;
+  p.pos += dt*p.vel;
+
+  mParticles[0] = p; 
+}
